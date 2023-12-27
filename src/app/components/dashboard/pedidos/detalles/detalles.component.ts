55b9790e157ad9894ea42de7f2a9360869fe64e5
jgarzon97 <jorgegarzonizquierdo@gmail.com>
2023-12-27 00:17:43 -0500

    cambios al components detaller

--- src/app/components/dashboard/pedidos/detalles/detalles.component.ts
+++ src/app/components/dashboard/pedidos/detalles/detalles.component.ts
@@ -1,10 +1,29 @@
 import { Component } from '@angular/core';
+import { Detalle, DetallesService } from 'src/app/services/pedido/detalles.service';
 
 @Component({
   selector: 'app-detalles',
   templateUrl: './detalles.component.html',
   styleUrls: ['./detalles.component.css']
 })
+
 export class DetallesComponent {
+  detalles: Detalle[] = [];
+
+  constructor(
+    private detallesService: DetallesService
+  ) { }
+
+  ngOnInit(): void {
+    this.visualizar();
+  }
+
+  visualizar() {
+    this.detallesService.getPedido_Producto().subscribe(response => {
+      this.detalles = response;
+    }, error => {
+      console.log(error);
+    });
+  }
 
 }
--- src/app/services/pedido/detalles.service.ts
+++ src/app/services/pedido/detalles.service.ts
@@ -1,9 +1,40 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class DetallesService {
 
-  constructor() { }
+  private apiUrl = 'http://localhost:3000'; // La URL base del servidor Express
+
+  constructor(private http: HttpClient) { }
+
+  getPedido_Producto(): Observable<Detalle[]> {
+    return this.http.get<Detalle[]>(`${this.apiUrl}/pedidoProductos`);
+  }
+
+  getPedido_Productos(id: number): Observable<Detalle> {
+    return this.http.get<Detalle>(`${this.apiUrl}/pedidoProducto/${id}`);
+  }
+
+  createPedido_Producto(pedidoData: Detalle): Observable<Detalle> {
+    return this.http.post<Detalle>(`${this.apiUrl}/pedidoProducto`, pedidoData);
+  }
+
+  updatePedido_Producto(id: number, pedidoData: Detalle): Observable<Detalle> {
+    return this.http.put<Detalle>(`${this.apiUrl}/pedidoProducto/${id}`, pedidoData);
+  }
+
+  deletePedido_Producto(id: number): Observable<Detalle> {
+    return this.http.delete<Detalle>(`${this.apiUrl}/pedidoProducto/${id}`);
+  }
+}
+
+export interface Detalle {
+  id_pedido: number;
+  id_producto: number;
+  cantidad: number;
+  detalle: string;
 }
