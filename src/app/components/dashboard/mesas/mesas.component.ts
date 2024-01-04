ac3e2b08d31cf7ff2c6698bfbfc4d2dfdb59bb22
jgarzon97 <jorgegarzonizquierdo@gmail.com>
2024-01-04 06:14:48 -0500

    permisos para el segundo rol

--- src/app/components/dashboard/mesas/mesas.component.ts
+++ src/app/components/dashboard/mesas/mesas.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
-import { Router } from '@angular/router';
+import { ActivatedRoute, Router } from '@angular/router';
 import { MesasService } from 'src/app/services/mesa/mesas.service';
+import { PedidosService } from 'src/app/services/pedido/pedidos.service';
 
 @Component({
   selector: 'app-mesas',
@@ -10,12 +11,19 @@ import { MesasService } from 'src/app/services/mesa/mesas.service';
 export class MesasComponent {
   mesas: any[] = [];
 
+  id_usuario: number | null = null;
+
   constructor(
-    private mesasService : MesasService,
-    private router: Router
-  ) { }
+    private mesasService: MesasService,
+    private pedidosService: PedidosService,
+    private router: Router,
+    private activatedRoute: ActivatedRoute
+  ) {
+    const mesaId = this.activatedRoute.snapshot.params['id_mesa'];
+  }
 
   ngOnInit(): void {
+    this.id_usuario = this.getUserIdFromLocalStorage();
     const isAuthenticated = localStorage.getItem('id_rol');
     if (!isAuthenticated) {
       console.log('No puedes ingresar, Inicia sesión');
@@ -25,11 +33,29 @@ export class MesasComponent {
     }
   }
 
+  getUserIdFromLocalStorage(): number | null {
+    const userIdString = localStorage.getItem('id_usuario');
+    return userIdString ? +userIdString : null;
+  }
+
   visualizar() {
     this.mesasService.getMesas().subscribe(response => {
       this.mesas = response;
     }, error => {
       console.log(error);
+    });
+  }
+
+  // Función para agregar un Pedido seleccionando una Mesa
+  crearPedido(id_usuario: number | null, mesas: any): void {
+    if (id_usuario === null) {
+      console.error('El id_usuario no está definido');
+      return;
+    }
+
+    this.pedidosService.createPedido(id_usuario, mesas).subscribe(() => {
+      alert('Se creó el pedido correctamente');
+      this.visualizar();
     });
   }
 }
--- src/app/components/dashboard/productos/productos.component.ts
+++ src/app/components/dashboard/productos/productos.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { Categoria, CategoriasService } from 'src/app/services/producto/categorias.service';
+import { Producto, ProductosService } from 'src/app/services/producto/productos.service';
 
 @Component({
   selector: 'app-productos',
@@ -6,5 +8,64 @@ import { Component } from '@angular/core';
   styleUrls: ['./productos.component.css']
 })
 export class ProductosComponent {
+  productos: Producto[] = [];
+  categorias: Categoria[] = [];
 
+  busqueda: string = '';
+  selectedRolId: number | undefined;
+
+  newCatego: any = {
+    nombre: null,
+  };
+
+  constructor(
+    private productoService : ProductosService,
+    private categoriasService: CategoriasService,
+  ) { }
+
+  ngOnInit(): void {
+    this.visualizar();
+  }
+
+  // Método para cargar todos los productos.
+  visualizar() {
+    this.productoService.getProductos().subscribe(response => {
+      this.productos = response;
+    }, error => {
+      console.log(error);
+    });
+
+    this.categoriasService.getCategoria().subscribe(response => {
+      this.categorias = response;
+    }, error => {
+      console.log(error);
+    });
+  }
+
+  // Método para filtrar los productos.
+  buscarProductos() {
+    if (this.busqueda.trim() === '') {
+      this.visualizar(); // Cargar todos los productos nuevamente si la búsqueda está vacía
+    } else {
+      const busquedaMinuscula = this.busqueda.toLowerCase();
+      this.productos = this.productos.filter(producto => 
+        producto.nombre.toLowerCase().includes(busquedaMinuscula)
+      );
+    }
+  }
+
+  addCategoria() {
+    this.categoriasService.createCategoria(this.newCatego).subscribe(
+      (respuesta: any) => {
+        console.log('Categoria creada:', respuesta);
+      },
+      (error: any) => {
+        console.error('Error al crear la categoria:', error);
+      }
+    );
+  }
+
+  onChange(event: any) {
+    this.selectedRolId = event.target.value;
+  }
 }
